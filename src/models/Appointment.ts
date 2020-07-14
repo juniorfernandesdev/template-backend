41f63ad70255ff208d5d8b684cc07b75652f2d8b
Junior Fernandes <juniorbass8@gmail.com>
2020-07-14 17:06:17 -0300

    Envio do projeto em pleno funcionamento

---
+++ src/models/Appointment.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+/**
+ * Um para um (One ToOne)
+ * Um para Muitos (OneToMany)
+ * Muitos para Muitos (ManyToMany)
+ */
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('time with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: string;
+
+  @UpdateDateColumn()
+  updated_at: string;
+}
+
+export default Appointment;
